321867e82beb25d179a26c8ecff96505300cf172
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-02-12 11:11:27 +1300

    Fix bug: apply `decode` to the parsed dictionaries

--- src/MLJRegistry.jl
+++ src/MLJRegistry.jl
@@ -4,10 +4,33 @@ export metadata, models
 
 import TOML
 
-const srcdir = dirname(@__FILE__) # the directory containing this file:
+const srcdir = dirname(@__FILE__) # the directory containing this file
+
+# for changing symbols to strings in dictionaries:
+encode(s) = s isa Symbol ? string(":", s) : s
+encode(v::Vector) = encode.(v)
+function encode(d::Dict)
+    ret = Dict{}()
+    for (k, v) in d
+        ret[encode(k)] = encode(v)
+    end
+    return ret
+end
+
+# for changing strings to symbols in dictionaries:
+decode(s) =
+    s isa String && !isempty(s) && s[1] == ':' ? Symbol(s[2:end]) : s
+decode(v::Vector) = decode.(v)
+function decode(d::Dict)
+    ret = Dict()
+    for (k, v) in d
+        ret[decode(k)] = decode(v)
+    end
+    return ret
+end
 
 # TODO: make these OS independent (../ not working on windows?)
-metadata() = TOML.parsefile(joinpath(srcdir, "../", "Metadata.toml"))
-models() = TOML.parsefile(joinpath(srcdir, "../", "Models.toml"))
+metadata() = TOML.parsefile(joinpath(srcdir, "../", "Metadata.toml")) |> decode
+models() = TOML.parsefile(joinpath(srcdir, "../", "Models.toml")) |> decode
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,15 @@
+# using Revise
 using Test
 using MLJRegistry
 
 @test !(isempty(metadata()))
 @test !(isempty(models()))
 
+d = Dict()
+d[:test] = "junk"
+d["junk"] = Dict{Any,Any}("H" => :fever, :cross => "lemon", :t => :w, "r" => "r")
+d["a"] = "b"
+d[:f] = :k
+d["j"] = :post
 
+@test MLJRegistry.decode(MLJRegistry.encode(d)) == d
