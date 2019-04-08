6daee8f78371157ef3572fc1c62f340e5f8d4956
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-04-09 11:20:49 +1200

    add XGBoost hack to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 # using Revise
 using Test
+
+# brittle hack b/s of https://github.com/dmlc/XGBoost.jl/issues/58:
+using Pkg
+Pkg.add(PackageSpec(url="https://github.com/dmlc/XGBoost.jl"))
+
 using MLJRegistry
 using MLJBase
 
